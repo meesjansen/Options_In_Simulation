52cafeba4cd222a69d47bb926082044332482c72
meesjansen <mees.thomas.jansen@gmail.com>
2024-09-18 12:50:55 +0200

    second attempt at working package

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="my_project",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=["assets", "robots", "tests", "utils", "assets.*", "robots.*", "tests.*", "utils.*"]),
     install_requires=[],  # Add your dependencies here if needed
 )
