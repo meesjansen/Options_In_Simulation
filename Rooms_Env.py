65ac45d64ad517fb1952e382413ae457a8e2da63
meesjansen <mees.thomas.jansen@gmail.com>
2024-09-20 09:34:03 +0200

    name correction

--- Rooms_Env.py
+++ Rooms_Env.py
@@ -31,7 +31,7 @@ TASK_CFG = {"test": False,
                              "enableDebugVis": False,
                              "clipObservations": 1000.0,
                              "controlFrequencyInv": 4,
-                             "basiInitState": {"pos": [0.0, 0.0, 0.62], # x,y,z [m]
+                             "baseInitState": {"pos": [0.0, 0.0, 0.62], # x,y,z [m]
                                               "rot": [1.0, 0.0, 0.0, 0.0], # w,x,y,z [quat]
                                               "vLinear": [0.0, 0.0, 0.0],  # x,y,z [m/s]
                                               "vAngular": [0.0, 0.0, 0.0],  # x,y,z [rad/s]
