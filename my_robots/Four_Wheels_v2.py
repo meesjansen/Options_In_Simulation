94a57039d93f00a2736f3dcfa2d8983fececb133
meesjansen <mees.thomas.jansen@gmail.com>
2024-09-20 17:59:44 +0200

    test7

--- my_robots/Four_Wheels_v2.py
+++ my_robots/Four_Wheels_v2.py
@@ -23,6 +23,7 @@ class LimoAckermann(WheeledRobot):
         wheel_dof_names: Optional[str] = None,
         position: Optional[torch.tensor] = None,
         orientation: Optional[torch.tensor] = None,
+        device: torch.device = torch.device('gpu')
     ) -> None:
         """Initialize the LimoAckermann robot with the appropriate drives and DoFs."""
         
