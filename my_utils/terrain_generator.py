0afbf19e7b858f1a98b899fbe5d89a54a3b22839
meesjansen <mees.thomas.jansen@gmail.com>
2024-09-20 10:10:10 +0200

    another indent fixed

--- my_utils/terrain_generator.py
+++ my_utils/terrain_generator.py
@@ -36,36 +36,36 @@ class Terrain:
                     self.height_field_raw, self.horizontal_scale, self.vertical_scale, slope_threshold=0.5)
 
 
-        def cr_rooms_env():
-            for k in range(self.num_maps):
-                # Env coordinates in the world
-                (i, j) = np.unravel_index(k, (self.env_rows, self.env_cols))
+    def cr_rooms_env(self):
+        for k in range(self.num_maps):
+            # Env coordinates in the world
+            (i, j) = np.unravel_index(k, (self.env_rows, self.env_cols))
 
-                # Heightfield coordinate system from now on
-                start_x = self.border + i * self.length_per_env_pixels
-                end_x = self.border + (i + 1) * self.length_per_env_pixels
-                start_y = self.border + j * self.width_per_env_pixels
-                end_y = self.border + (j + 1) * self.width_per_env_pixels
+            # Heightfield coordinate system from now on
+            start_x = self.border + i * self.length_per_env_pixels
+            end_x = self.border + (i + 1) * self.length_per_env_pixels
+            start_y = self.border + j * self.width_per_env_pixels
+            end_y = self.border + (j + 1) * self.width_per_env_pixels
 
-                terrain = SubTerrain(
-                    "terrain",
-                    width=self.width_per_env_pixels,
-                    length=self.width_per_env_pixels,
-                    vertical_scale=self.vertical_scale,
-                    horizontal_scale=self.horizontal_scale,
-                )
+            terrain = SubTerrain(
+                "terrain",
+                width=self.width_per_env_pixels,
+                length=self.width_per_env_pixels,
+                vertical_scale=self.vertical_scale,
+                horizontal_scale=self.horizontal_scale,
+            )
 
-                rooms_terrain(terrain, wall_height=100, wall_thickness=5, passage_width=20)
+            rooms_terrain(terrain, wall_height=100, wall_thickness=5, passage_width=20)
 
-                self.height_field_raw[start_x:end_x, start_y:end_y] = terrain.height_field_raw
+            self.height_field_raw[start_x:end_x, start_y:end_y] = terrain.height_field_raw
 
-                env_origin_x = (i + 0.5) * self.env_length
-                env_origin_y = (j + 0.5) * self.env_width
-                x1 = int((self.env_length / 2.0 - 1) / self.horizontal_scale)
-                x2 = int((self.env_length / 2.0 + 1) / self.horizontal_scale)
-                y1 = int((self.env_width / 2.0 - 1) / self.horizontal_scale)
-                y2 = int((self.env_width / 2.0 + 1) / self.horizontal_scale)
-                env_origin_z = np.max(terrain.height_field_raw[x1:x2, y1:y2]) * self.vertical_scale
-                self.env_origins[i, j] = [env_origin_x, env_origin_y, env_origin_z]
+            env_origin_x = (i + 0.5) * self.env_length
+            env_origin_y = (j + 0.5) * self.env_width
+            x1 = int((self.env_length / 2.0 - 1) / self.horizontal_scale)
+            x2 = int((self.env_length / 2.0 + 1) / self.horizontal_scale)
+            y1 = int((self.env_width / 2.0 - 1) / self.horizontal_scale)
+            y2 = int((self.env_width / 2.0 + 1) / self.horizontal_scale)
+            env_origin_z = np.max(terrain.height_field_raw[x1:x2, y1:y2]) * self.vertical_scale
+            self.env_origins[i, j] = [env_origin_x, env_origin_y, env_origin_z]
 
-                
+            
