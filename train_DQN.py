0ecca8288c5291e6e61f5014be6cf55a32a5cf99
meesjansen <mees.thomas.jansen@gmail.com>
2024-10-25 13:11:48 +0200

    num_env change

--- train_DQN.py
+++ train_DQN.py
@@ -29,7 +29,7 @@ from my_envs.DQN_terrain import ReachingTargetTask, TASK_CFG
 
 TASK_CFG["seed"] = seed
 TASK_CFG["headless"] = headless
-TASK_CFG["task"]["env"]["numEnvs"] = 25
+TASK_CFG["task"]["env"]["numEnvs"] = 16
 
 
 sim_config = SimConfig(TASK_CFG)
