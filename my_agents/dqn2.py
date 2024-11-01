327b9801fa78bd8a4aaa5e42a362ccc765a25b65
meesjansen <mees.thomas.jansen@gmail.com>
2024-11-01 16:28:35 +0100

    print everything

--- my_agents/dqn2.py
+++ my_agents/dqn2.py
@@ -287,6 +287,8 @@ class DQN(Agent):
         # sample a batch from memory
         sampled_states, sampled_actions, sampled_rewards, sampled_next_states, sampled_dones = \
             self.memory.sample(names=self.tensors_names, batch_size=self._batch_size)[0]
+        
+        print(sampled_states, sampled_actions, sampled_rewards, sampled_next_states, sampled_dones)
 
         # gradient steps
         for gradient_step in range(self._gradient_steps):
