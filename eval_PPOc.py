66bead133693aaf8f61d3e4eac56c627bc727cc0
meesjansen <mees.thomas.jansen@gmail.com>
2024-12-05 17:21:25 +0100

    next

--- eval_PPOc.py
+++ eval_PPOc.py
@@ -158,7 +158,7 @@ agent = PPO(models=models_ppo,
             action_space=env.action_space,
             device=device)
 
-agent.load("./my_runs/PPOc_r3/PPOc_r3/checkpoints/agent_200000.pt")
+agent.load("./my_runs/PPOc_06_r4/PPOc_o6_r4/checkpoints/agent_200000.pt")
 
 
 
