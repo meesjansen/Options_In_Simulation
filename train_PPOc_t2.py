80f2fda576021dce2a366a1fae91a5cb9c4a5105
meesjansen <mees.thomas.jansen@gmail.com>
2024-12-10 11:41:10 +0100

    correction

--- train_PPOc_t2.py
+++ train_PPOc_t2.py
@@ -64,7 +64,7 @@ headless = True  # set headless to False for rendering
 env = get_env_instance(headless=headless, enable_livestream=False, enable_viewport=False)
 
 from omniisaacgymenvs.utils.config_utils.sim_config import SimConfig
-from my_envs.PPOc_terrain_r5 import ReachingTargetTask, TASK_CFG
+from my_envs.PPOc_terrain_t2 import ReachingTargetTask, TASK_CFG
 
 TASK_CFG["seed"] = seed
 TASK_CFG["headless"] = headless
