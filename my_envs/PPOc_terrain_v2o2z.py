d7d238398d89d0e31d4212279a2cacc8775d6e2a
meesjansen <mees.thomas.jansen@gmail.com>
2024-12-12 16:48:21 +0100

    new

--- my_envs/PPOc_terrain_v2o2z.py
+++ my_envs/PPOc_terrain_v2o2z.py
@@ -196,8 +196,7 @@ class ReachingTargetTask(RLTask):
 
         super().set_up_scene(scene, collision_filter_global_paths=["/World/terrain"], copy_from_source=True)
 
-        light_prim = get_prim_at_path("/World/defaultDistantLight")
-        color=(1.0, 0.0, 1.0)
+        light_prim = self._stage.GetPrimAtPath("/World/defaultDistantLight")
         light_prim.CreateColorAttr(Gf.Vec3f(1.0, 0.0, 1.0))
 
         self._robots = RobotView(prim_paths_expr="/World/envs/.*/robot_*", name="robot_view")
