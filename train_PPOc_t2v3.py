149475e798714d3833ea303e4800bf8907d54f4c
meesjansen <mees.thomas.jansen@gmail.com>
2024-12-12 15:50:17 +0100

    livestream TRUE

--- train_PPOc_t2v3.py
+++ train_PPOc_t2v3.py
@@ -61,7 +61,7 @@ class Value(DeterministicMixin, Model):
 # instantiate and configure the task
 headless = True  # set headless to False for rendering
 
-env = get_env_instance(headless=headless, enable_livestream=False, enable_viewport=False)
+env = get_env_instance(headless=headless, enable_livestream=True, enable_viewport=True)
 
 from omniisaacgymenvs.utils.config_utils.sim_config import SimConfig
 from my_envs.PPOc_terrain_t2v3 import ReachingTargetTask, TASK_CFG
@@ -138,7 +138,7 @@ PPO_DEFAULT_CONFIG = {
         "store_separately": False,          # whether to store checkpoints separately
 
         "wandb": True,             # whether to use Weights & Biases
-        "wandb_kwargs": {"project":     "RL-Terrain-Simulation",
+        "wandb_kwargs": {"project":     "PPOc",
                         "entity":       "meesjansen-Delft Technical University",
                         "name":         "PPOc_Rooms_t2v3",
                         "tags":         ["PPOc", "Rooms"],
