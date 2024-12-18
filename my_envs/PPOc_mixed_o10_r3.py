4a3e46574ad31d1b6399f26c9ee91a1904ed3f67
meesjansen <mees.thomas.jansen@gmail.com>
2024-12-18 16:58:24 +0100

    update

--- my_envs/PPOc_mixed_o10_r3.py
+++ my_envs/PPOc_mixed_o10_r3.py
@@ -47,7 +47,7 @@ TASK_CFG = {"test": False,
                                                 },
                             "dofInitTorques": [0.0, 0.0, 0.0, 0.0],
                             "dofInitVelocities": [0.0, 0.0, 0.0, 0.0],
-                            "TerrainType": "rooms", # rooms, stairs, sloped, mixed_v1, mixed_v2, mixed_v3, custom, custom_mixed                         
+                            "TerrainType": "custom", # rooms, stairs, sloped, mixed_v1, mixed_v2, mixed_v3, custom, custom_mixed                         
 
                             },
                      "sim": {"dt": 0.0083,  # 1 / 120
@@ -123,7 +123,7 @@ class ReachingTargetTask(RLTask):
         self.dt = 1 / 120.0
 
         # observation and action space DQN
-        self._num_observations = 8  # features (+ height points)
+        self._num_observations = 10  # features (+ height points)
         self._num_actions = 4  # Designed discrete action space see pre_physics_step()
 
         self.observation_space = spaces.Box(
@@ -275,7 +275,7 @@ class ReachingTargetTask(RLTask):
     def get_target(self):
         target = DynamicSphere(prim_path=self.default_zero_env_path + "/target",
                                name="target",
-                               radius=0.1,
+                               radius=0.5,
                                color=torch.tensor([1, 0, 0]))
         self._sim_config.apply_articulation_settings("target", get_prim_at_path(target.prim_path), self._sim_config.parse_actor_config("target"))
         target.set_collision_enabled(False)
@@ -308,9 +308,9 @@ class ReachingTargetTask(RLTask):
         )
         self.num_dof = self._robots.num_dof 
         self.env_origins = self.terrain_origins.view(-1, 3)[:self.num_envs]
-        self._target_pos = torch.zeros((self.num_envs, 3), dtype=torch.float, device=self.device)
-        self._target_pos += torch.tensor([0.5, 2.0, 0.1], dtype=torch.float, device=self.device)
-        self._target_pos[:, :2] += self.env_origins[:, :2]
+        self.target_pos = torch.zeros((self.num_envs, 3), dtype=torch.float, device=self.device)
+        self.target_pos += torch.tensor([0.0, 0.0, 0.26], dtype=torch.float, device=self.device)
+        self.target_pos[:, :2] += self.env_origins[:, :2]
         self.base_velocities = torch.zeros((self.num_envs, 6), dtype=torch.float, device=self.device)
         self.dof_vel = torch.zeros((self.num_envs, self.num_dof), dtype=torch.float, device=self.device)
         self.dof_efforts = torch.zeros((self.num_envs, self.num_dof), dtype=torch.float, device=self.device)
@@ -318,7 +318,7 @@ class ReachingTargetTask(RLTask):
         indices = torch.arange(self._num_envs, dtype=torch.int64, device=self.device)
         self.reset_idx(indices)
         base_pos, base_quat = self._robots.get_world_poses(clone=False)
-        self.last_distance_to_target = torch.norm(base_pos - self._target_pos, dim=-1)
+        self.last_distance_to_target = torch.norm(base_pos - self.target_pos, dim=-1)
 
         self.init_done = True
 
@@ -330,7 +330,7 @@ class ReachingTargetTask(RLTask):
         square_size_x = 4.5  # Total width of the square
         square_size_y = 4.5  # Total length of the square
 
-        edge = random.randint(0, 3)
+        edge = random.randint(0, 1)
 
         # Generate x and y positions based on the edge
         if edge == 0:  # Left edge
@@ -339,16 +339,16 @@ class ReachingTargetTask(RLTask):
         elif edge == 1:  # Right edge
             x_pos = square_size_x / 2
             y_pos = random.uniform(-square_size_y / 2, square_size_y / 2)
-        elif edge == 2:  # Top edge
-            y_pos = square_size_y / 2
-            x_pos = random.uniform(-square_size_x / 2, square_size_x / 2)
-            if -0.5 < x_pos < 0.5:
-                x_pos = 0.5
-        else:  # Bottom edge
-            y_pos = -square_size_y / 2
-            x_pos = random.uniform(-square_size_x / 2, square_size_x / 2)
-            if -0.5 < x_pos < 0.5:
-                x_pos = 0.5
+        # elif edge == 2:  # Top edge
+        #     y_pos = square_size_y / 2
+        #     x_pos = random.uniform(-square_size_x / 2, square_size_x / 2)
+        #     if -0.5 < x_pos < 0.5:
+        #         x_pos = 0.5
+        # else:  # Bottom edge
+        #     y_pos = -square_size_y / 2
+        #     x_pos = random.uniform(-square_size_x / 2, square_size_x / 2)
+        #     if -0.5 < x_pos < 0.5:
+        #         x_pos = 0.5
 
         # Z position is fixed at 0.15
         z_pos = 0.15
@@ -380,13 +380,13 @@ class ReachingTargetTask(RLTask):
         self._robots.set_joint_efforts(self.dof_efforts[env_ids].clone(), indices=indices)
         self._robots.set_joint_velocities(velocities=self.dof_vel[env_ids].clone(), indices=indices)   
 
-        self._targets.set_world_poses(positions=self._target_pos[env_ids].clone(), indices=indices)
+        self._targets.set_world_poses(positions=self.target_pos[env_ids].clone(), indices=indices)
 
         self.last_actions[env_ids] = 0.0
         self.progress_buf[env_ids] = 0
         self.episode_buf[env_ids] = 0 
         # self.reset_buf[env_ids] = 0
-        
+
 
     def refresh_body_state_tensors(self):
         self.base_pos, self.base_quat = self._robots.get_world_poses(clone=False)
@@ -576,7 +576,7 @@ class ReachingTargetTask(RLTask):
         w_diff = 1.0
         delta_climb = torch.abs(current_efforts[:, 0] - current_efforts[:, 1]) + torch.abs(current_efforts[:, 2] - current_efforts[:, 3])
         w_climb = 1.0   
-        delta_torque = w_uniform * delta_uniform + w_diff * delta_diff  # + w_climb * delta_climb
+        delta_torque = w_uniform * delta_uniform + w_diff * delta_diff  + w_climb * delta_climb
 
         # Bonus for reaching the target
         target_reached = self.target_reached.float() * 1000.0
@@ -616,7 +616,7 @@ class ReachingTargetTask(RLTask):
                     self.base_vel[:, 0:3],
                     self.angle_difference.unsqueeze(-1),
                     self.projected_gravity,
-                    self._computed_distance.unsqueeze(-1),
+                    delta_pos,
                 ),
                 dim=-1,
             )
--- train_rooms_o10_r3_t2.py
+++ train_rooms_o10_r3_t2.py
@@ -64,7 +64,7 @@ headless = True  # set headless to False for rendering
 env = get_env_instance(headless=headless, enable_livestream=False, enable_viewport=False)
 
 from omniisaacgymenvs.utils.config_utils.sim_config import SimConfig
-from my_envs.PPOc_mixed_o8_r3 import ReachingTargetTask, TASK_CFG
+from my_envs.PPOc_rooms_o10_r3_t2 import ReachingTargetTask, TASK_CFG
 
 TASK_CFG["seed"] = seed
 TASK_CFG["headless"] = headless
@@ -130,8 +130,8 @@ PPO_DEFAULT_CONFIG = {
     "time_limit_bootstrap": False,  # bootstrap at timeout termination (episode truncation)
 
     "experiment": {
-        "directory": "/workspace/Options_In_Simulation/my_runs/PPOc_mixed_o8_r3",            # experiment's parent directory
-        "experiment_name": "PPOc_mixed_o8_r3",      # experiment name
+        "directory": "/workspace/Options_In_Simulation/my_runs/PPOc_rooms_o10_r3_t2",            # experiment's parent directory
+        "experiment_name": "PPOc_rooms_o10_r3_t2",      # experiment name
         "write_interval": "auto",   # TensorBoard writing interval (timesteps)
 
         "checkpoint_interval": "auto",      # interval for checkpoints (timesteps)
@@ -140,7 +140,7 @@ PPO_DEFAULT_CONFIG = {
         "wandb": True,             # whether to use Weights & Biases
         "wandb_kwargs": {"project":     "PPOc",
                         "entity":       "meesjansen-Delft Technical University",
-                        "name":         "PPOc_mixed_o8_r3",
+                        "name":         "PPOc_rooms_o10_r3_t2",
                         "tags":         ["PPOc", "Rooms"],
                         "dir":          "/workspace/Options_In_Simulation/my_runs"}    # wandb kwargs (see https://docs.wandb.ai/ref/python/init)
                     }          # wandb kwargs (see https://docs.wandb.ai/ref/python/init)
