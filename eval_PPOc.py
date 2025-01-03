9498ec9992b07346ddc349b444d52312721da401
meesjansen <mees.thomas.jansen@gmail.com>
2025-01-03 11:09:24 +0100

    eval

--- eval_PPOc.py
+++ eval_PPOc.py
@@ -46,7 +46,7 @@ headless = True  # set headless to False for rendering
 env = get_env_instance(headless=headless, enable_livestream=True, enable_viewport=True)
 
 from omniisaacgymenvs.utils.config_utils.sim_config import SimConfig
-from my_envs.PPOc_rooms_o10_r1 import ReachingTargetTask, TASK_CFG
+from my_envs.PPOc_rooms_o10_r1_a2 import ReachingTargetTask, TASK_CFG
 
 TASK_CFG["seed"] = seed
 TASK_CFG["headless"] = headless
@@ -158,7 +158,9 @@ agent = PPO(models=models_ppo,
             action_space=env.action_space,
             device=device)
 
-agent.load("./my_runs/PPOc_rooms_o10_r1/PPOc_rooms_o10_r1/checkpoints/agent_450000.pt")
+agent.load("./my_runs/PPOc_rooms_o10_r1_a2/PPOc_rooms_o10_r1_a2/checkpoints/agent_300000.pt")
+# agent.load("./my_runs/PPOc_rooms_o10_r4_t22_a2/PPOc_rooms_o10_r4_t22_a2/checkpoints/checkpoint_300000.pt")
+# agent.load("./my_runs/PPOc_terrain_r4_v21/PPOc_terrain_r4_v21/checkpoints/checkpoint_100000.pt")
 
 
 
