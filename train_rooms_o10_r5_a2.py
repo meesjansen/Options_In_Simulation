4de0823f926a8c45799a471daf2ba41401afb727
meesjansen <mees.thomas.jansen@gmail.com>
2025-01-06 18:18:19 +0100

    updates

--- train_rooms_o10_r5_a2.py
+++ train_rooms_o10_r5_a2.py
@@ -23,8 +23,8 @@ seed = set_seed(42)
 # - Policy: takes as input the environment's observation/state and returns action probs
 # - Value: takes the state as input and provides a state value to guide the policy
 class Policy(GaussianMixin, Model):
-    def __init__(self, observation_space, action_space, device, clip_actions=False,
-                 clip_log_std=True, min_log_std=-20, max_log_std=2):
+    def __init__(self, observation_space, action_space, device, clip_actions=True,
+                 clip_log_std=True, min_log_std=-20, max_log_std=0):
         Model.__init__(self, observation_space, action_space, device)
         GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std)
 
@@ -155,13 +155,13 @@ cfg_ppo["lambda"] = 0.95
 cfg_ppo["learning_rate"] = 5e-4
 cfg_ppo["learning_rate_scheduler"] = KLAdaptiveRL
 cfg_ppo["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
-cfg_ppo["random_timesteps"] = 0
+cfg_ppo["random_timesteps"] = 5000
 cfg_ppo["learning_starts"] = 0 # cfg_ppo["rollouts"] * env.num_envs * 4
 cfg_ppo["grad_norm_clip"] = 1.0
 cfg_ppo["ratio_clip"] = 0.2
 cfg_ppo["value_clip"] = 0.2
 cfg_ppo["clip_predicted_values"] = True
-cfg_ppo["entropy_loss_scale"] = 0.0
+cfg_ppo["entropy_loss_scale"] = 0.01
 cfg_ppo["value_loss_scale"] = 2.0
 cfg_ppo["kl_threshold"] = 0
 cfg_ppo["state_preprocessor"] = RunningStandardScaler
