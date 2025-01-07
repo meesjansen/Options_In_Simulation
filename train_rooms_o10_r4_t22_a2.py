c79b762102c91ea5ab73deb7ad653c2088a85502
meesjansen <mees.thomas.jansen@gmail.com>
2025-01-07 21:43:05 +0100

    viewport to False for headless HPC

--- train_rooms_o10_r4_t22_a2.py
+++ train_rooms_o10_r4_t22_a2.py
@@ -61,7 +61,7 @@ class Value(DeterministicMixin, Model):
 # instantiate and configure the task
 headless = True  # set headless to False for rendering
 
-env = get_env_instance(headless=headless, enable_livestream=True, enable_viewport=True)
+env = get_env_instance(headless=headless, enable_livestream=False, enable_viewport=False)
 
 from omniisaacgymenvs.utils.config_utils.sim_config import SimConfig
 from my_envs.PPOc_rooms_o10_r4_t22_a2 import ReachingTargetTask, TASK_CFG
