de44958fe7ed7d02cf8adddd05956c06cffa6ea6
meesjansen <mees.thomas.jansen@gmail.com>
2025-01-07 12:40:42 +0100

    AWS prep

--- train_rooms_o10_r1_a1.py
+++ train_rooms_o10_r1_a1.py
@@ -19,10 +19,12 @@ from my_agents.ppo import PPO
 seed = set_seed(42)
 
 
-# Define only the policy for evaluation
+# Define the models (stochastic and deterministic models) for the agent using helper mixin.
+# - Policy: takes as input the environment's observation/state and returns action probs
+# - Value: takes the state as input and provides a state value to guide the policy
 class Policy(GaussianMixin, Model):
-    def __init__(self, observation_space, action_space, device, clip_actions=False,
-                 clip_log_std=True, min_log_std=-20, max_log_std=2):
+    def __init__(self, observation_space, action_space, device, clip_actions=True,
+                 clip_log_std=True, min_log_std=-20, max_log_std=0):
         Model.__init__(self, observation_space, action_space, device)
         GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std)
 
@@ -39,14 +41,30 @@ class Policy(GaussianMixin, Model):
         return self.net(inputs["states"]), self.log_std_parameter, {}
 
 
+class Value(DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False):
+        Model.__init__(self, observation_space, action_space, device)
+        DeterministicMixin.__init__(self, clip_actions)
+
+        self.net = nn.Sequential(nn.Linear(self.num_observations, 256),
+                                 nn.ReLU(),
+                                 nn.Linear(256, 128),
+                                 nn.ReLU(),
+                                 nn.Linear(128, 64),
+                                 nn.ReLU(),
+                                 nn.Linear(64, 1))
+
+    def compute(self, inputs, role):
+        return self.net(inputs["states"]), {}
+
 
 # instantiate and configure the task
 headless = True  # set headless to False for rendering
 
-env = get_env_instance(headless=headless, enable_livestream=True, enable_viewport=True)
+env = get_env_instance(headless=headless, enable_livestream=False, enable_viewport=False)
 
 from omniisaacgymenvs.utils.config_utils.sim_config import SimConfig
-from my_envs.PPOc_rooms_o10_r1_a2 import ReachingTargetTask, TASK_CFG
+from my_envs.PPOc_rooms_o10_r1_a1 import ReachingTargetTask, TASK_CFG
 
 TASK_CFG["seed"] = seed
 TASK_CFG["headless"] = headless
@@ -64,7 +82,7 @@ env = wrap_env(env, "omniverse-isaacgym")
 device = env.device
 
 # instantiate a memory as experience replay 202
-# memory = RandomMemory(memory_size=20, num_envs=env.num_envs, device=device, replacement=False)
+memory = RandomMemory(memory_size=20, num_envs=env.num_envs, device=device, replacement=False)
 
 
 # Instantiate the agent's models (function approximators).
@@ -72,7 +90,7 @@ device = env.device
 # https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#spaces-and-models
 models_ppo = {}
 models_ppo["policy"] = Policy(env.observation_space, env.action_space, device)
-# models_ppo["value"] = Value(env.observation_space, env.action_space, device)
+models_ppo["value"] = Value(env.observation_space, env.action_space, device)
 
 
 # Configure and instantiate the agent.
@@ -95,7 +113,7 @@ PPO_DEFAULT_CONFIG = {
     "value_preprocessor": None,             # value preprocessor class (see skrl.resources.preprocessors)
     "value_preprocessor_kwargs": {},        # value preprocessor's kwargs (e.g. {"size": 1})
 
-    "random_timesteps": 0,          # random exploration steps
+    "random_timesteps": 5000,          # random exploration steps
     "learning_starts": 0,           # learning starts after this many steps
 
     "grad_norm_clip": 0.5,              # clipping coefficient for the norm of the gradients
@@ -103,7 +121,7 @@ PPO_DEFAULT_CONFIG = {
     "value_clip": 0.2,                  # clipping coefficient for computing the value loss (if clip_predicted_values is True)
     "clip_predicted_values": False,     # clip predicted values during value loss computation
 
-    "entropy_loss_scale": 0.0,      # entropy loss scaling factor
+    "entropy_loss_scale": 0.01,      # entropy loss scaling factor
     "value_loss_scale": 1.0,        # value loss scaling factor
 
     "kl_threshold": 0,              # KL divergence threshold for early stopping
@@ -112,15 +130,19 @@ PPO_DEFAULT_CONFIG = {
     "time_limit_bootstrap": False,  # bootstrap at timeout termination (episode truncation)
 
     "experiment": {
-        "directory": "",            # experiment's parent directory
-        "experiment_name": "",      # experiment name
+        "directory": "/workspace/Options_In_Simulation/my_runs/PPOc_rooms_o10_r1_a1",            # experiment's parent directory
+        "experiment_name": "PPOc_rooms_o10_r1_a1",      # experiment name
         "write_interval": "auto",   # TensorBoard writing interval (timesteps)
 
         "checkpoint_interval": "auto",      # interval for checkpoints (timesteps)
         "store_separately": False,          # whether to store checkpoints separately
 
-        "wandb": False,             # whether to use Weights & Biases
-        "wandb_kwargs": {}    # wandb kwargs (see https://docs.wandb.ai/ref/python/init)
+        "wandb": True,             # whether to use Weights & Biases
+        "wandb_kwargs": {"project":     "PPOc",
+                        "entity":       "meesjansen-Delft Technical University",
+                        "name":         "PPOc_rooms_o10_r1_a1",
+                        "tags":         ["PPOc", "Rooms"],
+                        "dir":          "/workspace/Options_In_Simulation/my_runs"}    # wandb kwargs (see https://docs.wandb.ai/ref/python/init)
                     }          # wandb kwargs (see https://docs.wandb.ai/ref/python/init)
     }
 
@@ -133,13 +155,13 @@ cfg_ppo["lambda"] = 0.95
 cfg_ppo["learning_rate"] = 5e-4
 cfg_ppo["learning_rate_scheduler"] = KLAdaptiveRL
 cfg_ppo["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
-cfg_ppo["random_timesteps"] = 0
+cfg_ppo["random_timesteps"] = 5000
 cfg_ppo["learning_starts"] = 0 # cfg_ppo["rollouts"] * env.num_envs * 4
 cfg_ppo["grad_norm_clip"] = 1.0
 cfg_ppo["ratio_clip"] = 0.2
 cfg_ppo["value_clip"] = 0.2
 cfg_ppo["clip_predicted_values"] = True
-cfg_ppo["entropy_loss_scale"] = 0.0
+cfg_ppo["entropy_loss_scale"] = 0.01
 cfg_ppo["value_loss_scale"] = 2.0
 cfg_ppo["kl_threshold"] = 0
 cfg_ppo["state_preprocessor"] = RunningStandardScaler
@@ -148,25 +170,20 @@ cfg_ppo["value_preprocessor"] = RunningStandardScaler
 cfg_ppo["value_preprocessor_kwargs"] = {"size": 1, "device": device}
 # logging to TensorBoard and write checkpoints
 cfg_ppo["experiment"]["write_interval"] = 500
-cfg_ppo["experiment"]["checkpoint_interval"] = 0
+cfg_ppo["experiment"]["checkpoint_interval"] = 50_000
 
 
 agent = PPO(models=models_ppo,
-            memory=None,
+            memory=memory,
             cfg=cfg_ppo,
             observation_space=env.observation_space,
             action_space=env.action_space,
             device=device)
 
-# agent.load("./my_runs/PPOc_rooms_o10_r1_a2/PPOc_rooms_o10_r1_a2/checkpoints/agent_300000.pt")
-agent.load("./my_runs/PPOc_rooms_o10_r4_t22_a2/PPOc_rooms_o10_r4_t22_a2/checkpoints/checkpoint_150000.pt")
-# agent.load("./my_runs/PPOc_terrain_r4_v21/PPOc_terrain_r4_v21/checkpoints/checkpoint_100000.pt")
-
-
 
 # Configure and instantiate the RL trainer
-cfg_trainer = {"timesteps": 50_000, "headless": True}
+cfg_trainer = {"timesteps": 500000, "headless": True}
 trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
 # start training
-trainer.eval()
+trainer.train()
