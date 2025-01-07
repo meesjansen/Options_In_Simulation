f3ab0690c6eb167e5b1d487d091e00fc592fe619
meesjansen <mees.thomas.jansen@gmail.com>
2025-01-07 14:55:03 +0100

    minor bug

--- eval_PPOc.py
+++ eval_PPOc.py
@@ -159,8 +159,8 @@ agent = PPO(models=models_ppo,
             device=device)
 
 # agent.load("./my_runs/PPOc_rooms_o10_r1_a2/PPOc_rooms_o10_r1_a2/checkpoints/agent_300000.pt")
-agent.load("./my_runs/PPOc_rooms_o10_r4_t22_a2/PPOc_rooms_o10_r4_t22_a2/checkpoints/checkpoint_150000.pt")
-# agent.load("./my_runs/PPOc_terrain_r4_v21/PPOc_terrain_r4_v21/checkpoints/checkpoint_100000.pt")
+agent.load("./my_runs/PPOc_rooms_o10_r4_t22_a2/PPOc_rooms_o10_r4_t22_a2/checkpoints/agent_150000.pt")
+# agent.load("./my_runs/PPOc_terrain_r4_v21/PPOc_terrain_r4_v21/checkpoints/agent_100000.pt")
 
 
 
