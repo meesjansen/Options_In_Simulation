69c8a2d5a7fef931740ec275f444d982dfd887c0
meesjansen <mees.thomas.jansen@gmail.com>
2025-01-08 11:59:28 +0100

    false viewport

--- my_envs/PPOc_rooms_o10_r2_a2.py
+++ my_envs/PPOc_rooms_o10_r2_a2.py
@@ -473,7 +473,7 @@ class ReachingTargetTask(RLTask):
 
         # print(f"Updated Efforts: {updated_efforts}")
         updated_efforts = torch.clip(updated_efforts, -15.0, 15.0)
-        print(f"Clipped Efforts: {updated_efforts}")
+        # print(f"Clipped Efforts: {updated_efforts}")
 
 
         if self.world.is_playing():
