5157b50de0b01b397fd0446ea8ffb685ac06982f
meesjansen <mees.thomas.jansen@gmail.com>
2025-01-10 09:43:43 +0100

    WandB names

--- train_rooms_o3_r3_a2.py
+++ train_rooms_o3_r3_a2.py
@@ -130,8 +130,8 @@ PPO_DEFAULT_CONFIG = {
     "time_limit_bootstrap": False,  # bootstrap at timeout termination (episode truncation)
 
     "experiment": {
-        "directory": "/workspace/Options_In_Simulation/my_runs/PPOc_rooms_o10_r3_a2",            # experiment's parent directory
-        "experiment_name": "PPOc_rooms_o10_r3_a2",      # experiment name
+        "directory": "/workspace/Options_In_Simulation/my_runs/PPOc_rooms_o3_r3_a2",            # experiment's parent directory
+        "experiment_name": "PPOc_rooms_o3_r3_a2",      # experiment name
         "write_interval": "auto",   # TensorBoard writing interval (timesteps)
 
         "checkpoint_interval": "auto",      # interval for checkpoints (timesteps)
@@ -140,7 +140,7 @@ PPO_DEFAULT_CONFIG = {
         "wandb": True,             # whether to use Weights & Biases
         "wandb_kwargs": {"project":     "PPOc",
                         "entity":       "meesjansen-Delft Technical University",
-                        "name":         "PPOc_rooms_o10_r3_a2",
+                        "name":         "PPOc_rooms_o3_r3_a2",
                         "tags":         ["PPOc", "Rooms"],
                         "dir":          "/workspace/Options_In_Simulation/my_runs"}    # wandb kwargs (see https://docs.wandb.ai/ref/python/init)
                     }          # wandb kwargs (see https://docs.wandb.ai/ref/python/init)
