c034ac4cd8f575df63ece3cd39be2564fb787b0e
meesjansen <mees.thomas.jansen@gmail.com>
2025-01-14 15:49:38 +0100

    changes

--- train_rooms_o5_r9_a2.py
+++ train_rooms_o5_r9_a2.py
@@ -24,7 +24,7 @@ seed = set_seed(42)
 # - Value: takes the state as input and provides a state value to guide the policy
 class Policy(GaussianMixin, Model):
     def __init__(self, observation_space, action_space, device, clip_actions=True,
-                 clip_log_std=True, min_log_std=-20, max_log_std=-4):
+                 clip_log_std=True, min_log_std=-6, max_log_std=0):
         Model.__init__(self, observation_space, action_space, device)
         GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std)
 
@@ -149,19 +149,19 @@ PPO_DEFAULT_CONFIG = {
 cfg_ppo = PPO_DEFAULT_CONFIG.copy()
 cfg_ppo["rollouts"] = 1280
 cfg_ppo["learning_epochs"] = 10
-cfg_ppo["mini_batches"] = 800
+cfg_ppo["mini_batches"] = 64
 cfg_ppo["discount_factor"] = 0.99
 cfg_ppo["lambda"] = 0.95
-cfg_ppo["learning_rate"] = 25e-5
+cfg_ppo["learning_rate"] = 5e-4
 cfg_ppo["learning_rate_scheduler"] = KLAdaptiveRL
-cfg_ppo["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
+cfg_ppo["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.02}
 cfg_ppo["random_timesteps"] = 0
-cfg_ppo["learning_starts"] = 0 # cfg_ppo["rollouts"] * env.num_envs * 4
+cfg_ppo["learning_starts"] = 0 
 cfg_ppo["grad_norm_clip"] = 1.0
 cfg_ppo["ratio_clip"] = 0.2
 cfg_ppo["value_clip"] = 0.2
 cfg_ppo["clip_predicted_values"] = True
-cfg_ppo["entropy_loss_scale"] = 0.01
+cfg_ppo["entropy_loss_scale"] = 0.03
 cfg_ppo["value_loss_scale"] = 0.5
 cfg_ppo["kl_threshold"] = 0
 cfg_ppo["state_preprocessor"] = RunningStandardScaler
