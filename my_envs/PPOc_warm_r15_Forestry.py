dacfe6d845fa92bea70e9fadb162620f8796d5be
meesjansen <mees.thomas.jansen@gmail.com>
2025-02-19 12:09:36 +0100

    expand

--- my_envs/PPOc_warm_r15_Forestry.py
+++ my_envs/PPOc_warm_r15_Forestry.py
@@ -166,7 +166,7 @@ class ReachingTargetTask(RLTask):
         self._env_spacing = 0.0
         
         # observation and action space (DQN)
-        self._num_observations = 163
+        self._num_observations = 162
         self._num_actions = 4  # Designed discrete action space see pre_physics_step()
 
         self.observation_space = spaces.Box(
@@ -442,7 +442,10 @@ class ReachingTargetTask(RLTask):
         self.base_pos[env_ids] = self.base_init_state[0:3]
         self.base_pos[env_ids, 0:3] += self.env_origins[env_ids]
         self.base_pos[env_ids, 0:2] += torch_rand_float(-0.5, 0.5, (len(env_ids), 2), device=self.device)
-        self.base_quat[env_ids] = self.base_init_state[3:7]
+        # Apply random rotation around Z-axis
+        random_angle = torch_rand_float(0, 2 * torch.pi, (len(env_ids),), device=self.device)
+        quat_z = torch.stack([torch.cos(random_angle / 2), torch.zeros_like(random_angle), torch.zeros_like(random_angle), torch.sin(random_angle / 2)], dim=-1)
+        self.base_quat[env_ids] = quat_mul(quat_z, self.base_init_state[3:7].repeat(len(env_ids), 1))
         self.base_velocities[env_ids] = self.base_init_state[7:]
 
 
@@ -685,7 +688,7 @@ class ReachingTargetTask(RLTask):
                 self.base_ang_vel * self.ang_vel_scale,
                 self.projected_gravity,
                 (self.commands[:, 0] * self.commands_scale[0]).unsqueeze(1),    # (num_envs, 1)
-                (self.commands[:, 2] * self.commands_scale[2]).unsqueeze(1),
+                # (self.commands[:, 2] * self.commands_scale[2]).unsqueeze(1),
                 self.dof_vel * self.r * self.dof_vel_scale,
                 self.actions,
                 self.lambda_slip * self.lambda_slip_scale,
