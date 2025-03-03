7effad331d98faa7fb658cdff04859e2f7839686
meesjansen <mees.thomas.jansen@gmail.com>
2025-03-03 13:13:07 +0100

    try

--- my_envs/PPOc_warm_r16_Forestry.py
+++ my_envs/PPOc_warm_r16_Forestry.py
@@ -337,13 +337,13 @@ class ReachingTargetTask(RLTask):
         self.get_robot()
 
         wheel_collision_paths = [
-            "/World/envs/env_0/robot/left_front_wheel",
-            "/World/envs/env_0/robot/right_front_wheel",
-            "/World/envs/env_0/robot/left_rear_wheel",
-            "/World/envs/env_0/robot/right_rear_wheel"
+            "/World/envs/env_0/robot/left_front_wheel/collisions",
+            "/World/envs/env_0/robot/right_front_wheel/collisions",
+            "/World/envs/env_0/robot/left_rear_wheel/collisions",
+            "/World/envs/env_0/robot/right_rear_wheel/collisions"
         ]
-        for path in wheel_collision_paths:
-            self._enable_custom_geometry_for_collision(path)
+        # for path in wheel_collision_paths:
+        #     self._enable_custom_geometry_for_collision(path)
 
         super().set_up_scene(scene, collision_filter_global_paths=["/World/terrain"], copy_from_source=True)
 
