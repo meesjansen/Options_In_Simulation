558579efea3409f5a61f7673e7c2cac76b9e152a
meesjansen <mees.thomas.jansen@gmail.com>
2025-03-03 13:07:01 +0100

    corrections

--- my_envs/PPOc_warm_r16_Forestry.py
+++ my_envs/PPOc_warm_r16_Forestry.py
@@ -337,10 +337,10 @@ class ReachingTargetTask(RLTask):
         self.get_robot()
 
         wheel_collision_paths = [
-            "/World/envs/env_0/robot/left_front_wheel/collision",
-            "/World/envs/env_0/robot/right_front_wheel/collision",
-            "/World/envs/env_0/robot/left_rear_wheel/collision",
-            "/World/envs/env_0/robot/right_rear_wheel/collision"
+            "/World/envs/env_0/robot/left_front_wheel/collisions",
+            "/World/envs/env_0/robot/right_front_wheel/collisions",
+            "/World/envs/env_0/robot/left_rear_wheel/collisions",
+            "/World/envs/env_0/robot/right_rear_wheel/collisions"
         ]
         for path in wheel_collision_paths:
             self._enable_custom_geometry_for_collision(path)
@@ -355,7 +355,7 @@ class ReachingTargetTask(RLTask):
     def _enable_custom_geometry_for_collision(self, collision_prim_path: str):
 
         collision_prim = get_prim_at_path(collision_prim_path)
-        
+
         if collision_prim:
             if not PhysxSchema.PhysxCollisionAPI.Has(collision_prim):
                 PhysxSchema.PhysxCollisionAPI.Apply(collision_prim)
