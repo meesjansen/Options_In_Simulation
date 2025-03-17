dc48cb20c2cd75628edb96097f196eafe0181359
meesjansen <mees.thomas.jansen@gmail.com>
2025-03-17 18:00:25 +0100

    update

--- train_KA-DDPG_r18.py
+++ train_KA-DDPG_r18.py
@@ -75,7 +75,7 @@ parsed_config = arg_parser.parse_args().__dict__
 
 TASK_CFG["seed"] = seed
 TASK_CFG["headless"] = headless
-TASK_CFG["task"]["env"]["numEnvs"] = 128
+TASK_CFG["task"]["env"]["numEnvs"] = 1
 
 # control
 TASK_CFG["task"]["env"]["control"]["stiffness"] = parsed_config["stiffness"]
