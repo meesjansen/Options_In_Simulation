257d7d70a19c1e5cc598cb6164fa53c994a0f141
meesjansen <mees.thomas.jansen@gmail.com>
2025-03-17 18:08:34 +0100

    update

--- my_envs/KA_DDPG_r18.py
+++ my_envs/KA_DDPG_r18.py
@@ -21,7 +21,7 @@ from omni.isaac.core.prims import GeometryPrim, GeometryPrimView
 from pxr import PhysxSchema, UsdPhysics
 
 
-from my_robots.origin_v18 import AvularOrigin_v10 as Robot_v10 
+from my_robots.origin_v10 import AvularOrigin_v10 as Robot_v10 
 
 from my_utils.origin_terrain_generator import *
 from my_utils.terrain_utils import *
@@ -584,7 +584,7 @@ class TorqueDistributionTask(RLTask):
         self.omega_delta = self.desired_omega - current_omega
 
 
-        self.Kp_omega = 0.05
+        self.Kp_omega = 0.1
         # Compute criteria actions for each wheel:
         # Left wheels get: Kp * ( (m*v_delta/dt) - (J*omega_delta/dt) )
         # Right wheels get: Kp * ( (m*v_delta/dt) + (J*omega_delta/dt) )
