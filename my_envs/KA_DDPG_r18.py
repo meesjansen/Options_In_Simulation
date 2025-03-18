2fa5d00f8fe6fa31b5f44dd9d0ec46515e18200c
meesjansen <mees.thomas.jansen@gmail.com>
2025-03-18 14:40:39 +0100

    update

--- my_envs/KA_DDPG_r18.py
+++ my_envs/KA_DDPG_r18.py
@@ -634,7 +634,8 @@ class TorqueDistributionTask(RLTask):
 
                 SimulationContext.step(self.world, render=False)
 
-        print("pre_physics; gamma_assist: ", self._robots.get_applied_joint_efforts())
+        print("pre_physics; applied efforts: ", self._robots.get_applied_joint_efforts(clone=False))
+        print("pre_physics; actions, still x100 for self.action_scale: ", self._robots.get_joint_velocities(clone=False))
 
         print("pre_physics; actions, still x100 for self.action_scale: ", self.actions[0])
         print("pre_physics; desired_v: ", self.desired_v[0])
