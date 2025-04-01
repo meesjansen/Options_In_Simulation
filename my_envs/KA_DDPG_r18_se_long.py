4c95f1102ec219ba9d7347fa4c644a845b872396
meesjansen <mees.thomas.jansen@gmail.com>
2025-04-01 14:46:00 +0200

    try

--- my_envs/KA_DDPG_r18_se_long.py
+++ my_envs/KA_DDPG_r18_se_long.py
@@ -649,7 +649,7 @@ class TorqueDistributionTask(RLTask):
         # print("pre_physics; expert torques left: ", self.ac_left[0])
         # print("pre_physics; expert torques right: ", self.ac_right[0])
         # print("pre_physics; executed torques pre clip: ", self.torques[0])
-        # print("pre_physics; executed torques post clip: ", self.wheel_torqs[0])
+        print("pre_physics; executed torques post clip: ", self.wheel_torqs[0])
         # print("base velocitites in z: ", self.base_velocities[0, 2])
 
           
