335a75411227008ae4de9e5da57819d63288d6e3
meesjansen <mees.thomas.jansen@gmail.com>
2025-04-15 09:52:30 +0200

    try

--- my_envs/KA_DDPG_r18_high_fid_2.py
+++ my_envs/KA_DDPG_r18_high_fid_2.py
@@ -764,14 +764,14 @@ class TorqueDistributionTask(RLTask):
         # r3: Torque penalty (sum of squared torques)
         r3 = torch.sum(self.wheel_torqs ** 2, dim=1)
         # Weight factors (tunable)
-        w1, w2, w3 = -90.0 , -0.6667, -0.36
+        w1, w2, w3 = -90.0 , -0.06667, -0.18
         rdense = w1 * r1 + w2 * r2 + w3 * r3
 
         # Sparse reward: bonus if tracking errors are very low
         sparse_reward = torch.where(
             (torch.abs(self.v_delta) < 0.01) &
             (torch.abs(self.omega_delta) < 0.01 ),
-            torch.full_like(self.v_delta, 1.0),
+            torch.full_like(self.v_delta, 2.0),
             torch.zeros_like(self.v_delta)
         )
         observed_reward = rdense + sparse_reward
