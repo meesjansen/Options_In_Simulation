677da95f39b8389902d16f2ca6497e92545e897c
meesjansen <mees.thomas.jansen@gmail.com>
2025-04-15 15:31:55 +0200

    try

--- my_envs/KA_DDPG_r18_high_fid_w.py
+++ my_envs/KA_DDPG_r18_high_fid_w.py
@@ -546,7 +546,7 @@ class TorqueDistributionTask(RLTask):
             # Random command generation
             x_vel = torch_rand_float(self.command_x_range[0], self.command_x_range[1], (1,1), device=self.device).squeeze()
             omega = torch_rand_float(self.command_yaw_range[0], self.command_yaw_range[1], (1,1), device=self.device).squeeze()
-            # x_vel = 0.0 # max 1.0
+            x_vel = 0.0 # max 1.0
             # omega = 0.0 # max 1.0
             return max(x_vel, 0.0), omega
         
@@ -586,7 +586,7 @@ class TorqueDistributionTask(RLTask):
         self.omega_delta = self.desired_omega - self.current_omega
 
 
-        self.Kp_omega = 1.0
+        self.Kp_omega = 10.0
         # Compute criteria actions for each wheel:
         # Left wheels get: Kp * ( (m*v_delta/dt) - (J*omega_delta/dt) )
         # Right wheels get: Kp * ( (m*v_delta/dt) + (J*omega_delta/dt) )
