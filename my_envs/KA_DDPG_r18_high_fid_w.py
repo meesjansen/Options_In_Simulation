9e9532f17984f56a5f55cbe214c95c1e28bd622b
meesjansen <mees.thomas.jansen@gmail.com>
2025-04-15 15:37:11 +0200

    try

--- my_envs/KA_DDPG_r18_high_fid_w.py
+++ my_envs/KA_DDPG_r18_high_fid_w.py
@@ -75,7 +75,7 @@ TASK_CFG = {"test": False,
                             "control": {"decimation": 10, # decimation: Number of control action updates @ sim DT per policy DT
                                         "stiffness": 1.0, # [N*m/rad] For torque setpoint control
                                         "damping": .005, # [N*m*s/rad]
-                                        "actionScale": 4.0,
+                                        "actionScale": 10.0,
                                         "wheel_radius": 0.1175,
                                         },   # leave room to overshoot or corner 
                             },
@@ -586,7 +586,7 @@ class TorqueDistributionTask(RLTask):
         self.omega_delta = self.desired_omega - self.current_omega
 
 
-        self.Kp_omega = 0.5
+        self.Kp_omega = 0.6
         # Compute criteria actions for each wheel:
         # Left wheels get: Kp * ( (m*v_delta/dt) - (J*omega_delta/dt) )
         # Right wheels get: Kp * ( (m*v_delta/dt) + (J*omega_delta/dt) )
@@ -632,7 +632,7 @@ class TorqueDistributionTask(RLTask):
         for _ in range(self.decimation):
             if self.world.is_playing():
                 
-                self.wheel_torqs = torch.clip(self.torques, -4.0, 4.0)
+                self.wheel_torqs = torch.clip(self.torques, -10.0, 10.0)
 
                 self._robots.set_joint_efforts(self.wheel_torqs)
 
@@ -650,7 +650,7 @@ class TorqueDistributionTask(RLTask):
         # print("pre_physics; expert torques right: ", self.ac_right[0])
         print("pre_physics; executed torques pre clip: ", self.torques[0])
         # print("pre_physics; executed torques post clip: ", self.wheel_torqs[0])
-        # print("base velocitites in z: ", self.base_velocities[0, 2])
+        print("base velocitites in z: ", self.base_velocities[0, 2])
 
           
     def post_physics_step(self):
