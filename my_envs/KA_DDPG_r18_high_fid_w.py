55ae0cd02aafaee20974694ec4ef1fb246ffafc5
meesjansen <mees.thomas.jansen@gmail.com>
2025-04-15 15:52:00 +0200

    try

--- my_envs/KA_DDPG_r18_high_fid_w.py
+++ my_envs/KA_DDPG_r18_high_fid_w.py
@@ -75,7 +75,7 @@ TASK_CFG = {"test": False,
                             "control": {"decimation": 10, # decimation: Number of control action updates @ sim DT per policy DT
                                         "stiffness": 1.0, # [N*m/rad] For torque setpoint control
                                         "damping": .005, # [N*m*s/rad]
-                                        "actionScale": 10.0,
+                                        "actionScale": 20.0,
                                         "wheel_radius": 0.1175,
                                         },   # leave room to overshoot or corner 
                             },
@@ -586,7 +586,7 @@ class TorqueDistributionTask(RLTask):
         self.omega_delta = self.desired_omega - self.current_omega
 
 
-        self.Kp_omega = 0.035
+        self.Kp_omega = 0.55
         # Compute criteria actions for each wheel:
         # Left wheels get: Kp * ( (m*v_delta/dt) - (J*omega_delta/dt) )
         # Right wheels get: Kp * ( (m*v_delta/dt) + (J*omega_delta/dt) )
