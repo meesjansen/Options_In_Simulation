51d4b6f4b1a14ba2bffd3438f774c91d283bd4fa
meesjansen <mees.thomas.jansen@gmail.com>
2025-04-21 20:52:33 +0200

    try

--- my_envs/KA_DDPG_r18_high_fid_10.py
+++ my_envs/KA_DDPG_r18_high_fid_10.py
@@ -414,7 +414,7 @@ class TorqueDistributionTask(RLTask):
         
 
     def reset_idx(self, env_ids):
-        
+        env_ids = env_ids.to(self.device)
         indices = env_ids.to(dtype=torch.int32)
 
         # self.update_SI_level(env_ids)
@@ -461,10 +461,17 @@ class TorqueDistributionTask(RLTask):
         self.episode_buf[env_ids] = 0 
         self.episode_count[env_ids] += 1
 
+        self.episode_sums["r1/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["r1: Tracking error reward (squared errors)"] / self.episode_sums["Final reward"])
+        self.episode_sums["r2/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["r2: Convergence reward (squared accelerations)"] / self.episode_sums["Final reward"])
+        self.episode_sums["r3/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["r3: Torque penalty (sum of squared torques)"] / self.episode_sums["Final reward"])
+        self.episode_sums["Dense/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["Dense reward"] / self.episode_sums["Final reward"])
+        self.episode_sums["Sparse/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["Sparse reward"] / self.episode_sums["Final reward"])
+        self.episode_sums["Guiding/Final reward"] = (100.0 * self.gamma_assist * self.episode_sums["Guiding reward"] / self.episode_sums["Final reward"])
+        self.episode_sums["Observed/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["Observed reward"] / self.episode_sums["Final reward"])
+
+
 
         # fill extras
-        env_ids = env_ids.to(self.device)           
-
         self.extras["episode"] = {}
         for key in self.episode_sums.keys():
             self.extras["episode"]["rew_" + key] = (
@@ -801,14 +808,7 @@ class TorqueDistributionTask(RLTask):
         self.episode_sums["Guiding reward"] += self.guiding_reward
         self.episode_sums["Observed reward"] += observed_reward
         self.episode_sums["Final reward"] += self.rew_buf
-        self.episode_sums["r1/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["r1: Tracking error reward (squared errors)"] / self.episode_sums["Final reward"]).cpu()
-        self.episode_sums["r2/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["r2: Convergence reward (squared accelerations)"] / self.episode_sums["Final reward"]).cpu()
-        self.episode_sums["r3/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["r3: Torque penalty (sum of squared torques)"] / self.episode_sums["Final reward"]).cpu()
-        self.episode_sums["Dense/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["Dense reward"] / self.episode_sums["Final reward"]).cpu()
-        self.episode_sums["Sparse/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["Sparse reward"] / self.episode_sums["Final reward"]).cpu()
-        self.episode_sums["Guiding/Final reward"] = (100.0 * self.gamma_assist * self.episode_sums["Guiding reward"] / self.episode_sums["Final reward"]).cpu()
-        self.episode_sums["Observed/Final reward"] = (100.0 * (1 - self.gamma_assist) * self.episode_sums["Observed reward"] / self.episode_sums["Final reward"]).cpu()
-        
+                
         self.comp_1 = w1 * r1
         self.comp_2 = w2 * r2
         self.comp_3 = w3 * r3
