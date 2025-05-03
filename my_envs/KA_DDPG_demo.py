dd3be42a49191dd32a3957d3eb1b8c79731be8bd
meesjansen <mees.thomas.jansen@gmail.com>
2025-05-03 17:32:37 +0200

    try

--- my_envs/KA_DDPG_demo.py
+++ my_envs/KA_DDPG_demo.py
@@ -615,7 +615,7 @@ class TorqueDistributionTask(RLTask):
         self.omega_delta = self.desired_omega - self.current_omega
 
 
-        self.Kp_omega = 10.0
+        self.Kp_omega = 5.0
         # Compute criteria actions for each wheel:
         # Left wheels get: Kp * ( (m*v_delta/dt) - (J*omega_delta/dt) )
         # Right wheels get: Kp * ( (m*v_delta/dt) + (J*omega_delta/dt) )
