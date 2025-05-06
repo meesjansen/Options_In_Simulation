9acc8559847c5c1ad665afe4948e53d60b47d937
meesjansen <mees.thomas.jansen@gmail.com>
2025-05-06 08:24:46 +0200

    print

--- my_envs/KAMMA_g1d_g21.py
+++ my_envs/KAMMA_g1d_g21.py
@@ -666,8 +666,8 @@ class TorqueDistributionTask(RLTask):
         # print("pre_physics; current_omega: ", self.current_omega[0])
         # print("pre_physics; expert torques left: ", self.ac_left[0])
         # print("pre_physics; expert torques right: ", self.ac_right[0])
-        print("pre_physics; executed torques pre clip: ", self.torques[0])
-        print("pre_physics; executed torques post clip: ", self.wheel_torqs[0])
+        # print("pre_physics; executed torques pre clip: ", self.torques[0])
+        # print("pre_physics; executed torques post clip: ", self.wheel_torqs[0])
         # print("base velocitites in z: ", self.base_velocities[0, 2])
 
           
