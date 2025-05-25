d57def000e63e6bfe986bcc30ac60f3a2c056931
meesjansen <mees.thomas.jansen@gmail.com>
2025-05-25 11:43:25 +0200

    try

--- eval_KAMMA.py
+++ eval_KAMMA.py
@@ -61,7 +61,7 @@ headless = True  # set headless to False for rendering
 env = get_env_instance(headless=headless, enable_livestream=False, enable_viewport=False)
 
 from omniisaacgymenvs.utils.config_utils.sim_config import SimConfig
-from my_envs.KA_DDPG_1D_eval import TorqueDistributionTask, TASK_CFG
+from my_envs.KAMMA_eval import TorqueDistributionTask, TASK_CFG
 from argparse import ArgumentParser 
 
 arg_parser = ArgumentParser()
@@ -146,15 +146,15 @@ DDPG_DEFAULT_CONFIG = {
     "mixed_precision": False,       # enable automatic mixed precision for higher performance
 
     "experiment": {
-        "directory": "/workspace/Options_In_Simulation/my_runs/KA_DDPG_1D_eval",
-        "experiment_name": "KA_DDPG_1D_eval",
+        "directory": "/workspace/Options_In_Simulation/my_runs/KAMMA_eval",
+        "experiment_name": "KAMMA_eval",
         "write_interval": "auto",
         "checkpoint_interval": "auto",
         "store_separately": False,
         "wandb": True,
         "wandb_kwargs": {"project": "KA-DDPG Dimension Study",
                          "entity": "meesjansen-Delft Technical University",
-                         "name": "KA_DDPG_1D_eval",
+                         "name": "KAMMA_eval",
                          "tags": ["DDPG", "KAMMA", "r18", "o6", "torq"],
                          "dir": "/workspace/Options_In_Simulation/my_runs"}    
                     }
@@ -186,7 +186,7 @@ agent = DDPG(models=models,
 
 
 # agent.load("./my_runs/PPOc_rooms_r15_vel/PPOc_rooms_r15_vel/checkpoints/agent_100000.pt")
-agent.load("./my_runs/KA-DDPG_1D/KA-DDPG_1D/checkpoints/agent_700000.pt")
+agent.load("./my_runs/KAMMA_medium/KAMMA_medium/checkpoints/agent_500000.pt")
 
 # Configure and instantiate the RL trainer
 cfg_trainer = {"timesteps": 50_000, "headless": True}
