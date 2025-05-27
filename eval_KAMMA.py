cb32ca04589db9d5577f7c2d2256847ca2b4d220
meesjansen <mees.thomas.jansen@gmail.com>
2025-05-27 10:07:31 +0200

    try

--- eval_KAMMA.py
+++ eval_KAMMA.py
@@ -146,15 +146,15 @@ DDPG_DEFAULT_CONFIG = {
     "mixed_precision": False,       # enable automatic mixed precision for higher performance
 
     "experiment": {
-        "directory": "/workspace/Options_In_Simulation/my_runs/Controller_eval",
-        "experiment_name": "controller_eval",
+        "directory": "/workspace/Options_In_Simulation/my_runs/KAMMA_eval_seed1",
+        "experiment_name": "KAMMA_eval_seed1",
         "write_interval": "auto",
         "checkpoint_interval": "auto",
         "store_separately": False,
         "wandb": True,
         "wandb_kwargs": {"project": "KAMMA",
                          "entity": "meesjansen-Delft Technical University",
-                         "name": "Controller_eval",
+                         "name": "KAMMA_eval_seed1",
                          "tags": ["DDPG", "KAMMA", "r18", "o6", "torq"],
                          "dir": "/workspace/Options_In_Simulation/my_runs"}    
                     }
