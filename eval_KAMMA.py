1532f003b37171b104a2b53607c75953d35228f9
meesjansen <mees.thomas.jansen@gmail.com>
2025-06-05 11:53:52 +0200

    try

--- eval_KAMMA.py
+++ eval_KAMMA.py
@@ -146,15 +146,15 @@ DDPG_DEFAULT_CONFIG = {
     "mixed_precision": False,       # enable automatic mixed precision for higher performance
 
     "experiment": {
-        "directory": "/workspace/Options_In_Simulation/my_runs/KAMMA_g1d_g21_seed3_eval",
-        "experiment_name": "KAMMA_g1d_g21_seed3_eval",
+        "directory": "/workspace/Options_In_Simulation/my_runs/KAMMA_g1d_g21_seed4_eval",
+        "experiment_name": "KAMMA_g1d_g21_seed4_eval",
         "write_interval": "auto",
         "checkpoint_interval": "auto",
         "store_separately": False,
         "wandb": True,
         "wandb_kwargs": {"project": "KAMMA",
                          "entity": "meesjansen-Delft Technical University",
-                         "name": "KAMMA_g1d_g21_seed3_eval",
+                         "name": "KAMMA_g1d_g21_seed4_eval",
                          "tags": ["DDPG", "KAMMA", "r18", "o6", "torq"],
                          "dir": "/workspace/Options_In_Simulation/my_runs"}    
                     }
@@ -186,7 +186,7 @@ agent = DDPG(models=models,
 
 
 # agent.load("./my_runs/PPOc_rooms_r15_vel/PPOc_rooms_r15_vel/checkpoints/agent_100000.pt")
-agent.load("./my_runs/KAMMA_g1d_g21_seed3/KAMMA_g1d_g21_seed3/checkpoints/agent_500000.pt")
+agent.load("./my_runs/KAMMA_g1d_g21_seed4/KAMMA_g1d_g21_seed4/checkpoints/agent_500000.pt")
 
 # Configure and instantiate the RL trainer
 cfg_trainer = {"timesteps": 50_000, "headless": True}
